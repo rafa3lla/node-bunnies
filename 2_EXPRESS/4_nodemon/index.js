83d2b48e2d3fdee2fb5ba2f6004bb4ee014e9ebf
Rafaella Silveira <rafaellagelsilveira@gmail.com>
2023-09-29 10:15:34 -0300

    maria joaquina

---
+++ 2_EXPRESS/4_nodemon/index.js
@@ -0,0 +1,20 @@
+// módulos internos, core modules e módulos externos
+
+const express = require("express")
+const path = require("path")
+
+
+const app = express()
+
+const caminho = path.join(__dirname, 'templates')
+
+app.get('/', (requisicao, resposta) => {
+    resposta.sendFile(`${caminho}/index.html`)
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000.")
+})
+
+
+// localhost:3000
